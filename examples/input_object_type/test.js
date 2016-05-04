8d52304af69a01ab241c90efadf8b5631632b8fe
Kazuhito Hokamura <k.hokamura@gmail.com>
2016-05-04 11:21:54 +0900

    add test

--- examples/input_object_type/test.js
+++ examples/input_object_type/test.js
@@ -10,4 +10,12 @@ describe('input_object_type', () => {
       assert(result.data.echo === 'x');
     });
   });
+
+  it('works', () => {
+    let query = 'query q($x: String) { echo(foo: { bar: { baz: $x } }) }';
+
+    return graphql(schema, query, null, null, { x: 1 }).then(result => {
+      assert(result.data.echo === '1');
+    });
+  });
 });
